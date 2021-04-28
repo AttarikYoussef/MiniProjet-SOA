1bffe54a13aea2697ba0a1034e3c326d3cd2e81c
AttarikYoussef <attarikyoussef8@gmail.com>
2021-04-28 22:57:15 +0100

    version 2

--- src/main/java/com/example/demo/Service/CreditCardService.java
+++ src/main/java/com/example/demo/Service/CreditCardService.java
@@ -2,16 +2,23 @@ package com.example.demo.Service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entity.Compte;
 import com.example.demo.Repository.CompteRepository;
+
 
 
 
@@ -21,10 +28,20 @@ public class CreditCardService {
 	@Autowired
 	CompteRepository compteRepository;
 
-	
-	public boolean authentification() {
-		return true;
+	@Produces(MediaType.TEXT_PLAIN)
+	@RequestMapping(path ="/auth/{p_email}/{p_pass}", method = RequestMethod.GET)
+	//public boolean authentification(@RequestParam(name = "email", required = false) String  p_email,@RequestParam(name = "pass", required = false) String  pass) {
+	public boolean authentification(@PathVariable String p_email, @PathVariable String p_pass) {	
+		System.out.println(p_pass);
+		Compte cp = compteRepository.findCompte(p_email);
+		if(cp!=null) {
+			return true;
+		}else {
+			return false;
+		}	
 	}
+	
+	
 	
 	@Produces(MediaType.APPLICATION_JSON)
 	@GetMapping("/Listobject")
@@ -32,4 +49,20 @@ public class CreditCardService {
 		return compteRepository.findAll();
 	}
 	
+	
+	@PostMapping(path = "/members", consumes = "application/json", produces = "application/json")
+	public boolean addMember(@RequestBody String member , HttpServletRequest request) {
+		
+		/*String[] params = member.split("&");     
+	    for (String param : params) {           
+	       System.out.println(param.split("=")[0]+" : "+param.split("=")[1]);
+	    }*/
+		System.out.println("==================================================");
+		System.out.println("Json Paquet : "+member);
+		System.out.println("Votre Adresse IP : "+request.getRemoteAddr());
+		System.out.println("Votre Session : "+request.getSession().getId());
+		System.out.println("==================================================");
+	    return true;
+	}
+
 }
